68eaa8f4f681edf0ab92afe9565f24994f0e7583
Liz <lizlarkin@highlandtechnology.com>
2021-10-19 22:22:38 -0700

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
-const path = require("path");
+const PORT = process.env.PORT || 3000;
+const path = require("path")
 
 require("./models/connection")
 
