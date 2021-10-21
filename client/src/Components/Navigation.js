52a4fbd2456177f8422cd298627c367a67c0f2d7
Liz <lizlarkin@highlandtechnology.com>
2021-10-20 19:27:02 -0700

    fixed nav hamburger

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -5,13 +5,14 @@ import "./navigation.css";
 const Navigation = () => {
     return (
         <div>
+
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse nav-fill" id="navbarSupportedContent">
-                        <ul className="nav nav-fill">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="nav">
                             <li className="nav-item" id="nav-icon">
                                 <Link to ="/" className="nav-link">JOY</Link>
                             </li>
