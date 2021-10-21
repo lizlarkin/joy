4383d4b8ccfe7b9f72980e988b674b1ff67dd5e2
Liz <lizlarkin@highlandtechnology.com>
2021-10-20 19:51:17 -0700

    fixed nav for real

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -7,9 +7,9 @@ const Navigation = () => {
         <div>
 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
+                <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="nav">
@@ -32,6 +32,7 @@ const Navigation = () => {
                     </div>
                 </div>
             </nav>
+            
         </div>
     )
 }
